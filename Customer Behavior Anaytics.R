c271c803cc81e1d47e992c219de35b0300f145db
dorthy1993 <35855193+dorthy1993@users.noreply.github.com>
2019-05-10 22:12:23 -0700

    Update Customer Behavior Anaytics.R

--- Customer Behavior Anaytics.R
+++ Customer Behavior Anaytics.R
@@ -1,35 +1,68 @@
-data_read<-read.csv(file.choose())
+
+setwd("/Users/DoryChen/Desktop")
+
+data_read <- read.csv("HighNote Data Midterm.csv", header=T)
+
+#install packages
+
+library(MatchIt)
+library(pastecs)
+library(dplyr)
+library(ggplot2)
+library(rmarkdown)
+
+
+#Pre-analysis 
+data_read$subscriber_friend <- ifelse(data_read$subscriber_friend_cnt == 0, 0,1)
+table(data_read$subscriber_friend)
+t.test(data_read$adopter~data_read$subscriber_friend)
+
+adopter_cov <- c('age', 'male', 'friend_cnt', 'avg_friend_age','avg_friend_male', 'friend_country_cnt',"subscriber_friend_cnt","songsListened",'lovedTracks',"posts",'playlists',"shouts",'tenure','good_country')
+lapply(adopter_cov, function(v) {
+  t.test(data_read[, v] ~ data_read$adopter)
+})
+
+
+# statistic descriptive 
 
 adopt<-filter(data_read,adopter==1)
 non_adopter<-filter(data_read,adopter==0)
 
-install.packages("pastecs")
-library(pastecs)
-
-# statistic descriptive 
-
 summary_adopt<-stat.desc(adopt)
 summary_non_adopt<-stat.desc(non_adopter)
 
-# logistic regression
+summary_adopt
+summary_non_adopt
 
-data_read$subscriber_friend<-ifelse(data_read$subscriber_friend_cnt==0,0,1)
+# Propensity Score Estimation
+# Estimate the propensity score by running a logistic model
 
-mylogit<-glm(adopter~ friend_cnt + avg_friend_age + avg_friend_male + subscriber_friend + friend_country_cnt+ songsListened+lovedTracks+posts+playlists+shouts,data=data_read,family=binomial())
+mylogit<-glm(adopter~ age + male + friend_cnt + avg_friend_age + avg_friend_male + subscriber_friend + friend_country_cnt + songsListened
+                      + lovedTracks + posts + playlists + shouts + tenure + good_country,data=data_read,family=binomial())
+
 
 summary(mylogit)
-exp(coef(mylogit))
 
+mylogit_reduced<-glm(adopter~ +age + male + avg_friend_age + subscriber_friend + friend_country_cnt 
+                     + songsListened + lovedTracks + playlists + tenure + good_country,data=data_read,family=binomial())
 
+exp(coef(mylogit_reduced))
 
-prs_df <- data.frame(pr_score = predict(mylogit, type = "response"),
-                     adopter=mylogit$model$adopter)
+prs_df <- data.frame(pr_score = predict(mylogit_reduced, type = "response"),
+                     adopter=mylogit_reduced$model$adopter)
 head(prs_df)
-head(mylogit_reduce$model)
+head(mylogit_reduced$model)
 
-#plot
+# Users whoes friends are subscribers are more likely to become premium users.
+# According to odd ratios, one more friend who are paid users the user has,
+# odds ratio goes up to 2.66 times
 
-labs <- paste(":", c("subscriber-friend", "no-subscreiber-friend"))
+# The model calculate the propensity score for each student.
+# It is the student's predicted probability of being treated.
+
+#plot histograms of the estimated propensity scores by treatment status
+
+labs <- paste("Actual subscriber_friend_cnt", c("SFC > 0", "SFC = 0"))
 
 prs_df %>%
   mutate(adopter = ifelse(adopter == 1, labs[1], labs[2])) %>%
@@ -40,22 +73,11 @@ prs_df %>%
   theme_bw()
 
 #PSM 
+# The method we use below is to find pairs of observations that have very similar propensity scores, 
+# but that differ in their treatment status. We use the package MatchIt for this. 
+# This package estimates the propensity score in the background and then matches observations based 
+# on the method of choice (“nearest” in this case).
 
-attach(data_read)
-
-data_read$subscriber_friend<-ifelse(data_read$subscriber_friend_cnt==0,0,1)
-
-t.test(data_read$adopter~data_read$subscriber_friend)
-
-install.packages("MatchIt")
-library(MatchIt)
-
-library(dplyr)
-
-adopter_cov <- c('age', 'male', 'friend_cnt', 'avg_friend_age','avg_friend_male', 'friend_country_cnt',"subscriber_friend_cnt","songsListened",'lovedTracks',"posts",'playlists',"shouts",'tenure','good_country')
-lapply(adopter_cov, function(v) {
-  t.test(data_read[, v] ~ data_read$adopter)
-})
 
 adopter_nomiss <- data_read %>%  # MatchIt does not allow missing values
   select(adopter, one_of(adopter_cov)) %>%
@@ -64,10 +86,14 @@ adopter_nomiss <- data_read %>%  # MatchIt does not allow missing values
 
 mod_match_adopt <- matchit(subscriber_friend ~ age + male + friend_cnt + avg_friend_age + avg_friend_male 
                            + friend_country_cnt+ songsListened+lovedTracks+posts+playlists+shouts+tenure+good_country
-                           , method = "nearest", data = data_read)
+                           , method = "nearest", data =data_read)
+
+# How successful the matching was using summary(mod_match) and plot(mod_match)
 
 summary(mod_match_adopt)
 plot(mod_match_adopt)
+
+# To create a dataframe containing only the matched observations, use the match.data() function
 
 dta_m_adopt <- match.data(mod_match_adopt)
 
